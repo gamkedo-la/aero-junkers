a303a222cbf30cc77445a5682e6b0356fe9e248b
brianjboucher <brianb823@gmail.com>
2021-06-23 17:56:23 -0500

    Renamed root node to Main. Added mouse capture on screen click

--- Scripts/aerojunker.gd
+++ Scripts/aerojunker.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 	velocity = Vector3.ZERO
 	get_input(delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
-		
+
 func get_input(delta):
 	if Input.is_action_pressed("turn_left"):
 		turnLeft(delta)
@@ -17,16 +17,16 @@ func get_input(delta):
 		moveForward(delta)
 	if Input.is_action_pressed("reverse"):
 		moveBackward(delta)
-		
+
 func moveForward(_delta):
 	velocity -= transform.basis.z * speed
-	
+
 func moveBackward(_delta):
 	velocity += transform.basis.z * speed
-	
+
 func turnLeft(_delta):
 	rotate_y(0.2)
-	
+
 func turnRight(_delta):
 	rotate_y(-0.2)
 
