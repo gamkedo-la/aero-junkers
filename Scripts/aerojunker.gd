d1f8f8e1bb0037b5fa7d5cc116e602b4f149278f
brianjboucher <brianb823@gmail.com>
2021-07-03 09:41:26 -0500

    changed speed from units/frame to units/second

--- Scripts/aerojunker.gd
+++ Scripts/aerojunker.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
-var speed = 50
-var altitude_adjust_speed = speed/3
+var speed = 3000 #units/second
+var altitude_adjust_speed = 30 #units/second
 var velocity = Vector3.ZERO
 var target_altitude = Vector3.UP * 3
 
@@ -18,10 +18,6 @@ func _physics_process(delta):
 	moveToTargetAltitude(delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
-	#Debug
-	$RayCast_L_Engine/MeshInstance.global_transform.origin.y = $RayCast_L_Engine.get_collision_point().y
-#	l_engine_height = $RayCast_L_Engine.get_distance_to_collision(delta)
-	
 func _process(_delta):
 	pass
 
@@ -34,28 +30,18 @@ func get_input(delta):
 		moveForward(delta)
 	if Input.is_action_pressed("reverse"):
 		moveBackward(delta)
-	if Input.is_action_pressed("increase_altitude"):
-		increaseAltitude(delta)
-	if Input.is_action_pressed("decrease_altitude"):
-		decreaseAltitude(delta)
-		
-func moveForward(_delta):
-	velocity -= transform.basis.z * speed
 
-func moveBackward(_delta):
-	velocity += transform.basis.z * speed
-
+func moveForward(delta):
+	velocity -= transform.basis.z * speed * delta
+	
+func moveBackward(delta):
+	velocity += transform.basis.z * speed * delta
+	
 func turnLeft(_delta):
 	rotate_y(0.2)
 
 func turnRight(_delta):
 	rotate_y(-0.2)
-	
-func increaseAltitude(_delta):
-	velocity += transform.basis.y * (speed/2)
-	
-func decreaseAltitude(_delta):
-	velocity -= transform.basis.y * (speed/2)
 	
 func moveToTargetAltitude(delta):
 	var target_position_y = ($RayCast_L_Engine.get_collision_point() + target_altitude).y
