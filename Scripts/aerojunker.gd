f420dff89f6bad6d0c5c6f8829172c479d0c38c7
brianjboucher <brianb823@gmail.com>
2021-07-03 12:25:21 -0500

    added gradual acceleration and deceleration

--- Scripts/aerojunker.gd
+++ Scripts/aerojunker.gd
@@ -1,47 +1,44 @@
 extends KinematicBody
 
-var speed = 3000 #units/second
-var altitude_adjust_speed = 30 #units/second
+var max_speed = 4000 #units/second
+var altitude_adjust_speed = 5 #units/second
+var acceleration = Vector3.ZERO
+var acceleration_direction = 0
 var velocity = Vector3.ZERO
 var target_altitude = Vector3.UP * 3
-
-var l_engine_height = 0
 
 func _ready():
 	if not CheckpointSingleton.is_connected("checkpoint_reached", self, "_player_reached_checkpoint"):
 		assert(CheckpointSingleton.connect("checkpoint_reached", self, "_player_reached_checkpoint") == OK)
-	pass
 
 func _physics_process(delta):
-	velocity = Vector3.ZERO
-	get_input(delta)
-	moveToTargetAltitude(delta)
-	velocity = move_and_slide(velocity, Vector3.UP)
 	
-func _process(_delta):
-	pass
+	get_input(delta)
+	apply_acceleration(delta)
+	
+	#change velocity from global to local orientation
+	velocity = global_transform.basis.orthonormalized().xform(velocity)
+	
+	move_and_slide(velocity, Vector3.UP)
+	moveToTargetAltitude(delta)
 
 func get_input(delta):
 	if Input.is_action_pressed("turn_left"):
-		turnLeft(delta)
+		rotate_y(0.2)
 	if Input.is_action_pressed("turn_right"):
-		turnRight(delta)
+		rotate_y(-0.2)
 	if Input.is_action_pressed("accelerate"):
-		moveForward(delta)
+		acceleration_direction = -1
 	if Input.is_action_pressed("reverse"):
-		moveBackward(delta)
-
-func moveForward(delta):
-	velocity -= transform.basis.z * speed * delta
-	
-func moveBackward(delta):
-	velocity += transform.basis.z * speed * delta
-	
-func turnLeft(_delta):
-	rotate_y(0.2)
-
-func turnRight(_delta):
-	rotate_y(-0.2)
+		acceleration_direction = 1
+	if Input.is_action_just_released("accelerate"):
+		acceleration_direction = 0
+	if Input.is_action_just_released("reverse"):
+		acceleration_direction = 0
+		
+func apply_acceleration(delta):
+	acceleration.z += ((max_speed * acceleration_direction) - acceleration.z) * delta
+	velocity = acceleration * delta
 	
 func moveToTargetAltitude(delta):
 	var target_position_y = ($RayCast_L_Engine.get_collision_point() + target_altitude).y
