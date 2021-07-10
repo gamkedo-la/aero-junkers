19cc4abbff5d0ac7fa4765f4106f698553b090ee
brianjboucher <brianb823@gmail.com>
2021-07-10 10:05:37 -0500

    Clean up unused code from aerojunker.gd

--- Scripts/aerojunker.gd
+++ Scripts/aerojunker.gd
@@ -3,13 +3,12 @@ extends KinematicBody
 
 signal switch_cam
 
-var max_speed = 4000 #units/second
-var altitude_adjust_speed = 5 #units/second
-var acceleration = Vector3.ZERO
-var acceleration_direction = 0
-var turn_direction = 0
-var velocity = Vector3.ZERO
-var target_altitude = Vector3.UP * 3
+var max_speed: float = 4000.0 #units/second
+var altitude_adjust_speed: float = 5.0 #units/second
+var acceleration: Vector3 = Vector3.ZERO
+var acceleration_direction: int = 0
+var velocity: Vector3 = Vector3.ZERO
+var target_altitude: Vector3 = Vector3.UP * 3
 
 # Follow Cam Variables
 onready var followcam: Camera = $ChaseCam
@@ -18,10 +17,6 @@ var camera_positions = []
 var cam_lerps = [11.5, 15.0, 20.0, 20.0, 20.0]
 var cur_camera_idx:int = 0
 
-var base_length = 5 #used as a "wheel base" length
-var max_turning_angle = 15
-var current_turning_angle = 0
-
 enum TURN_DIRECTION {LEFT, RIGHT}
 const MAX_ENGINE_ROTATION_ANGLE = 23
 
@@ -29,20 +24,17 @@ func _ready():
 	if not CheckpointSingleton.is_connected("checkpoint_reached", self, "_player_reached_checkpoint"):
 		assert(CheckpointSingleton.connect("checkpoint_reached", self, "_player_reached_checkpoint") == OK)
 	_init_follow_cam()
-	
-	
+
+
 func _init_follow_cam() -> void:
 	camera_positions = campos_node.get_children()
 	# Initialize Chase Camera
 	assert(followcam.initialize(self) == true)
 	emit_signal("switch_cam", camera_positions[cur_camera_idx], cam_lerps[cur_camera_idx])
-	
+
 
 func _physics_process(delta):
-
 	get_input(delta)
-	#current_turning_angle = turn_direction * deg2rad(max_turning_angle)
-	#calculate_turning(delta)
 	apply_acceleration(delta)
 	
 	#change velocity from global to local orientation
@@ -51,84 +43,68 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP)
 	moveToTargetAltitude(delta)
 
+
 func get_input(delta):
+	#Button Pressed
 	if Input.is_action_pressed("turn_left"):
-#		turn_direction = -1
 		rotate_y(0.02)
 		calculate_turn_engines(0.02, TURN_DIRECTION.LEFT)
 	if Input.is_action_pressed("turn_right"):
-#		turn_direction = 1
 		rotate_y(-0.02)
 		calculate_turn_engines(-0.02, TURN_DIRECTION.RIGHT)
 	if Input.is_action_pressed("accelerate"):
 		acceleration_direction = -1
 	if Input.is_action_pressed("reverse"):
 		acceleration_direction = 1
+		
+	#Button Released
 	if Input.is_action_just_released("accelerate"):
 		acceleration_direction = 0
 	if Input.is_action_just_released("reverse"):
 		acceleration_direction = 0
 	if Input.is_action_just_released("turn_left"):
-#		turn_direction = 0
 		reset_engine_rotation()
 	if Input.is_action_just_released("turn_right"):
-#		turn_direction = 0
 		reset_engine_rotation()
-
+	
 	# Camera Input
 	if Input.is_action_just_pressed("cam_toggle"):
 		_toggle_camera_up()
 	if Input.is_action_just_pressed("chase_cam"):
 		cur_camera_idx = 0
 		emit_signal("switch_cam", camera_positions[cur_camera_idx], cam_lerps[cur_camera_idx])
-		
-func calculate_turning(delta):
-	var engine_position: Vector3 = Vector3.ZERO
-	var cockpit_position: Vector3 = $MeshInstance_Cockpit.transform.origin
-	
-	$Cockpit_Pos.transform.origin = cockpit_position
-	$Engine_Pos.transform.origin = engine_position
-	
-	cockpit_position += velocity * delta
-	engine_position += velocity.rotated(transform.basis.y, current_turning_angle) * delta
-	
-	var new_heading: Vector3 = cockpit_position.direction_to(engine_position)
-	
-	var d = new_heading.dot(velocity.normalized())
-	if d > 0:
-		velocity = new_heading * velocity.length()
-	if d < 0:
-		velocity = new_heading * min(velocity.length(), max_speed)
-	look_at(transform.origin + new_heading, transform.basis.y)
-	
-	velocity = new_heading * velocity
-	#rotation = new_heading.angle()
 
-func calculate_turn_engines(radians, direction):
+
+func calculate_turn_engines(radians, direction) -> void:
 	if (direction == TURN_DIRECTION.RIGHT && $MeshInstance_R_Engine.rotation_degrees.y > -MAX_ENGINE_ROTATION_ANGLE):
 		rotate_engines_y(radians)
 	elif (direction == TURN_DIRECTION.LEFT && $MeshInstance_R_Engine.rotation_degrees.y < MAX_ENGINE_ROTATION_ANGLE):
 		rotate_engines_y(radians)
 
-func rotate_engines_y(radians):
+
+func rotate_engines_y(radians) -> void:
 	$MeshInstance_L_Engine.rotate_y(radians)
 	$CollisionShape_L_Engine.rotate_y(radians)
 	$MeshInstance_R_Engine.rotate_y(radians)
 	$CollisionShape_R_Engine.rotate_y(radians)
 
-func reset_engine_rotation():
+
+func reset_engine_rotation() -> void:
 	$MeshInstance_R_Engine.rotation_degrees.y = 0.0
 	$MeshInstance_L_Engine.rotation_degrees.y = 0.0
 	$CollisionShape_R_Engine.rotation_degrees.y = 0.0
 	$CollisionShape_L_Engine.rotation_degrees.y = 0.0
-	
-func apply_acceleration(delta):
+
+
+func apply_acceleration(delta) -> void:
 	acceleration.z += ((max_speed * acceleration_direction) - acceleration.z) * delta
 	velocity = acceleration * delta
-	
-func moveToTargetAltitude(delta):
+
+
+func moveToTargetAltitude(delta) -> void:
 	var target_position_y = ($RayCast_L_Engine.get_collision_point() + target_altitude).y
 	transform.origin.y = transform.origin.y + (target_position_y - transform.origin.y) * delta * altitude_adjust_speed
+
 
 func _toggle_camera_up() -> void:
 	if cur_camera_idx < camera_positions.size() - 1:
@@ -137,5 +113,6 @@ func _toggle_camera_up() -> void:
 		cur_camera_idx = 0
 	emit_signal("switch_cam", camera_positions[cur_camera_idx], cam_lerps[cur_camera_idx])
 
-func _player_reached_checkpoint():
+
+func _player_reached_checkpoint() -> void:
 	print("Player Reached Checkpoint")
