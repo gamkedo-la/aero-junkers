0a36288d6d6a8d51f6250ba4650cc976213baf45
TheHaad <fmuntaz@gmail.com>
2021-07-15 21:30:57 -0400

    [WIP] add environment collision detection

--- Scripts/aerojunker.gd
+++ Scripts/aerojunker.gd
@@ -10,6 +10,7 @@ var acceleration_direction = 0
 var turn_direction = 0
 var velocity = Vector3.ZERO
 var target_altitude = Vector3.UP * 3
+var health = 100
 
 # Follow Cam Variables
 onready var followcam: Camera = $ChaseCam
@@ -50,6 +51,14 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	moveToTargetAltitude(delta)
+	
+	for index in range(get_slide_count()):
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("environment"):
+			health -= 10
+			print('health: %d'	% health)
+	
+	
 
 func get_input(delta):
 	if Input.is_action_pressed("turn_left"):
