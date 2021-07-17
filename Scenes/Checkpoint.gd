15fbfb14fb59da2de421279758c31e08d7e60fbd
brianjboucher <brianb823@gmail.com>
2021-07-17 12:33:34 -0500

    Created scene Track1 and created a large terrain model

--- Scenes/Checkpoint.gd
+++
@@ -1,8 +0,0 @@
-extends Area
-
-func _ready():
-	if not self.is_connected("body_entered", self, "_checkpoint_entered"):
-		assert(self.connect("body_entered", self, "_checkpoint_entered") == OK)
-		
-func _checkpoint_entered(_body: Node) -> void:
-	CheckpointSingleton.emit_signal("checkpoint_reached")
