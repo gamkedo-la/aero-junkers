b0c69ddaab7aaae924b73bcc289c4d9b64e6dcc3
brianjboucher <brianb823@gmail.com>
2021-08-15 06:24:17 -0500

    Created title screen scene structure

---
+++ Scripts/UI/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Scenes/Tracks/Main.tscn")
+
+
+func _on_OptionsButton_pressed():
+	pass
+
+
+func _on_CreditsButton_pressed():
+	pass
