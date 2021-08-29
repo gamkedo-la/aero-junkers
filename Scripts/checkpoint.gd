05587cff1750e64df15967d1917279942ff771eb
brianjboucher <brianb823@gmail.com>
2021-08-29 10:01:30 -0500

    Commented out checkpoint debug print lines

--- Scripts/checkpoint.gd
+++ Scripts/checkpoint.gd
@@ -10,8 +10,8 @@ func _ready():
 
 func _checkpoint_entered(_body: Node) -> void:
 	if is_finish_line:
-		print_debug("Finish Line emitting signal")
+#		print_debug("Finish Line emitting signal")
 		CheckpointSingleton.emit_signal("finish_line_reached", self, _body)
 	
-	print_debug("Checkpoint emitting signal")
+#	print_debug("Checkpoint emitting signal")
 	CheckpointSingleton.emit_signal("checkpoint_reached", self, _body)
