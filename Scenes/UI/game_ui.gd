b142bc17275840e68296f206bb3212df4273b219
brianjboucher <brianb823@gmail.com>
2021-09-07 22:30:52 -0500

    Results screen shows positions of all AeroJunkers. Increased number of AeroJunkers to 6

--- Scenes/UI/game_ui.gd
+++ Scenes/UI/game_ui.gd
@@ -8,6 +8,11 @@ func _on_AeroJunker_new_lap(lap: int):
 	if lap > 3:
 		$EndScreen.visible = true
 		$EndScreen/Menu/ResultsText.bbcode_text = "[center]1st Place: %s[/center]" % CheckpointSingleton.positions[0].pilotName
+		$EndScreen/Menu/ResultsText2.bbcode_text = "[center]2nd Place: %s[/center]" % CheckpointSingleton.positions[1].pilotName
+		$EndScreen/Menu/ResultsText3.bbcode_text = "[center]3rd Place: %s[/center]" % CheckpointSingleton.positions[2].pilotName
+		$EndScreen/Menu/ResultsText4.bbcode_text = "[center]4th Place: %s[/center]" % CheckpointSingleton.positions[3].pilotName
+		$EndScreen/Menu/ResultsText5.bbcode_text = "[center]5th Place: %s[/center]" % CheckpointSingleton.positions[4].pilotName
+		$EndScreen/Menu/ResultsText6.bbcode_text = "[center]6th Place: %s[/center]" % CheckpointSingleton.positions[5].pilotName
 		print_debug("show results")
 	else:
 		$MarginContainer/ScreenVB/TopBar/Right/LapCounterLabel.bbcode_text = "[right]Lap %s/3 [/right]" % lap
@@ -15,4 +20,4 @@ func _on_AeroJunker_new_lap(lap: int):
 func _on_Checkpoint_Singleton_positions_updated(positions):
 	for i in positions.size():
 		if not positions[i].is_ai_controlled:
-			$MarginContainer/ScreenVB/TopBar/Left/PositionLabel.bbcode_text = "%s/8" % (i + 1)
+			$MarginContainer/ScreenVB/TopBar/Left/PositionLabel.bbcode_text = "%s/6" % (i + 1)
