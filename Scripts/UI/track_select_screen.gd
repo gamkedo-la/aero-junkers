a694e5dcd296d5290d2810002a2e1ccb8736e2fa
brianjboucher <brianb823@gmail.com>
2021-09-12 12:06:30 -0500

    Fixed issue with tundra rock wall textures

--- Scripts/UI/track_select_screen.gd
+++ Scripts/UI/track_select_screen.gd
@@ -5,7 +5,7 @@ func _on_Track1Button_pressed():
 
 
 func _on_Track2Button_pressed():
-	get_tree().change_scene("res://Scenes/Tracks/tundra.tscn")
+	get_tree().change_scene("res://Scenes/Tracks/tundra_track.tscn")
 
 
 func _on_Track3Button_pressed():
