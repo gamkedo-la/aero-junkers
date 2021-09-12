0543f121ae2e4e17c30981b8d2f5286f5cd39ccf
brianjboucher <brianb823@gmail.com>
2021-09-12 17:00:29 -0500

    Space and Q only change the scene when on the results screen. Changed results screen text

--- Scripts/UI/end_screen.gd
+++ Scripts/UI/end_screen.gd
@@ -6,10 +6,10 @@ func _on_TitleScreenButton_pressed():
 	Global.goto_scene("res://Scenes/UI/TitleScreen.tscn")
 
 func _process(delta):
-	if Input.is_action_just_released("return_to_title_screen"):
+	if Input.is_action_just_released("return_to_title_screen") and self.visible:
 		Global.emit_signal("return_to_menu")
 		Global.goto_scene("res://Scenes/UI/TitleScreen.tscn")
 		
-	if Input.is_action_just_released("next_race"):
+	if Input.is_action_just_released("next_race") and self.visible:
 		Global.emit_signal("return_to_menu")
 		Global.goto_scene("res://Scenes/Tracks/tundra_track.tscn")
